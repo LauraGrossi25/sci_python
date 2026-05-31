bdb3bf28d08ce8597ef51c6fde1d9cc73f5cbd39
LauraGrossi25 <grossi.laura.04@gmail.com>
2026-05-31 15:37:46 -0300

    SCI ADC DAC Timer funcionando

--- microcontroller/main.c
+++ microcontroller/main.c
@@ -9,6 +9,8 @@
 volatile Protocol_Header_t g_prot_header = {CMD_NONE,0};
 volatile int g_dado;
 
+__interrupt void INT_myADC0_1_ISR(void);
+
 //
 // Fun��o Principal
 //
@@ -65,4 +67,14 @@ __interrupt void INT_SCI0_RX_ISR(void)
 __interrupt void INT_myCPUTIMER0_ISR(void)
 {
     Interrupt_clearACKGroup(INT_myCPUTIMER0_INTERRUPT_ACK_GROUP);
+}
+
+__interrupt void INT_myADC0_1_ISR(void)
+{
+    ADC_clearInterruptStatus(
+        myADC0_BASE,
+        ADC_INT_NUMBER1);
+
+    Interrupt_clearACKGroup(
+        INT_myADC0_1_INTERRUPT_ACK_GROUP);
 }
